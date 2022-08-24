aa43114ed840e4a44db3257751d3bce1e1187d60
Matheus <matheuscelento@gmail.com>
2022-08-24 20:26:36 -0300

    Matheussalvo4

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -3,3 +3,8 @@ let matheuscelento@gmail.com
 let jogador de futebol
 let 21
 let Bahamas
+Matheus = celento
+matheuscelento@gmail.com = email
+jogador de futebol = profissao
+21 = 2001
+Bahamas = lugar 
